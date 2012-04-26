e3235d53ff54fa36e258ace2e15a24437ae85e8f
Christian Holler <decoder@mozilla.com>
2012-04-26 19:16:11 +0200

    Add mail configuration stuff Fix a config bug

--- adbfuzzconfig.py
+++ adbfuzzconfig.py
@@ -21,20 +21,24 @@ class ADBFuzzConfig:
 
   def __init__(self, cfgFile):
     cfgDefaults = {}
+    cfgDefaults['id'] = None
     cfgDefaults['remoteHost'] = None
     cfgDefaults['localPort'] = '8088'
     cfgDefaults['useWebSockets'] = False
     cfgDefaults['localWebSocketPort'] = '8089'
     cfgDefaults['libDir'] = None
     cfgDefaults['debug'] = str(False)
+    
+    cfgDefaults['useMail'] = str(False)
 
-    cfgDefaults['runTimeout'] = 5
+    cfgDefaults['runTimeout'] = str(5)
     cfgDefaults['maxLogSize'] = str(1024*1024*10) # Default to 10 kb maximum log
 
     self.cfg = SafeConfigParser(cfgDefaults)
     if (len(self.cfg.read(cfgFile)) == 0):
       raise Exception("Unable to read configuration file: " + cfgFile)
 
+    self.id = self.cfg.get('main', 'id')
     self.fuzzerFile = self.cfg.get('main', 'fuzzer')
     self.runTimeout = self.cfg.getint('main', 'runTimeout')
     self.maxLogSize = self.cfg.getint('main', 'maxLogSize')
@@ -47,6 +51,13 @@ class ADBFuzzConfig:
     self.useWebSockets = self.cfg.getboolean('main', 'useWebSockets')
     self.localWebSocketPort = self.cfg.get('main', 'localWebSocketPort')
     self.libDir = self.cfg.get('main', 'libDir')
+    
+    # Mail configuration
+    self.useMail = self.cfg.getboolean('main', 'useMail')
+    if self.useMail:
+      self.mailFrom = self.cfg.get('main', 'mailFrom')
+      self.mailTo = self.cfg.get('main', 'mailTo')
+      self.SMTPHost = self.cfg.get('main', 'SMTPHost')
 
 if __name__ == "__main__":
   raise Exception("This module cannot run standalone, but is used by ADBFuzz")
